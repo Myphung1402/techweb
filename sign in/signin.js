1dde9d8f54c9e5d15f2747697113be0759352eab
tuananhlehuy <tuananhbadaovl@gmail.com>
2020-04-18 20:38:10 +0700

    abc

---
+++ sign in/signin.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBLehoen_3ECaH49R3otGCWLpxOujgR5uc",
+    authDomain: "techweb-17c29.firebaseapp.com",
+    databaseURL: "https://techweb-17c29.firebaseio.com",
+    projectId: "techweb-17c29",
+    storageBucket: "techweb-17c29.appspot.com",
+    messagingSenderId: "607479175084",
+    appId: "1:607479175084:web:163e9fadc2468bca908c09",
+    measurementId: "G-FYX2ZYWJL7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
