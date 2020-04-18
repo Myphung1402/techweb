752e89cb7f292b2d9e9d0eedba4ce0f2f55dcbae
khanhdo1027 <62701333+khanhdo1027@users.noreply.github.com>
2020-04-18 17:20:16 +0700

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -1,56 +1,43 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyBLehoen_3ECaH49R3otGCWLpxOujgR5uc",
-  authDomain: "techweb-17c29.firebaseapp.com",
-  databaseURL: "https://techweb-17c29.firebaseio.com",
-  projectId: "techweb-17c29",
-  storageBucket: "techweb-17c29.appspot.com",
-  messagingSenderId: "607479175084",
-  appId: "1:607479175084:web:163e9fadc2468bca908c09",
-  measurementId: "G-FYX2ZYWJL7"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    apiKey: "AIzaSyBLehoen_3ECaH49R3otGCWLpxOujgR5uc",
+    authDomain: "techweb-17c29.firebaseapp.com",
+    databaseURL: "https://techweb-17c29.firebaseio.com",
+    projectId: "techweb-17c29",
+    storageBucket: "techweb-17c29.appspot.com",
+    messagingSenderId: "607479175084",
+    appId: "1:607479175084:web:163e9fadc2468bca908c09",
+    measurementId: "G-FYX2ZYWJL7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-var userDatabase = firebase.database().ref('/users')
+  var userDatabase = firebase.database().ref('/users')
 
-var contentDatabase = firebase.database().ref('/data')
+  var contentDatabase = firebase.database().ref('/data')
 
+  function register() {
+    const newUser = userDatabase.push()
+    newUser.set(
+      {
+        username: "khanhdo1027",
+        password: "khanhdo",
+        fullname: "Khanh Do",
+        address: "Hanoi"
+      }
+    )
 
-var userData
+    console.log("Done")
+  }
 
-function register() {
-  const newUser = userDatabase.push()
-  newUser.set(
-    {
-      username: "c4t-b01",
-      password: "admin",
-      fullname: "c4t-b01",
-      address: "Ha Noi"
-    }
-  )
-
-  console.log("Done")
-}
-
-function addNewData() {
-  const newData = contentDatabase.push()
-  newData.set(
-    {
-      title: "Javascript",
-      content: "javascript",
-
-    }
-  )
-}
-
-
-function getAllData () {
-  return userDatabase.on('value', function (snapshot) {
-    console.log(snapshot.val())
-    userData = snapshot.val()
-    return snapshot.val()
-  })
-}
-
-console.log(getAllData())
+  function addNewData() {
+    const newData = contentDatabase.push()
+    newData.set(
+      {
+        title:"abcxyz",
+        content:"Loremngjsdjh",
+        image: "",
+        thumbnail:"",
+      }
+    )
+  }
