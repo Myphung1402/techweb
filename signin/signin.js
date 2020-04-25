0aa8bff5a114d0ab81e244a7f0c1d7ccd80a7839
namnguyen6595 <namnguyen@beeketing.net>
2020-04-25 19:38:46 +0700

    update login

--- signin/signin.js
+++ signin/signin.js
@@ -1,25 +1,48 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBLehoen_3ECaH49R3otGCWLpxOujgR5uc",
-    authDomain: "techweb-17c29.firebaseapp.com",
-    databaseURL: "https://techweb-17c29.firebaseio.com",
-    projectId: "techweb-17c29",
-    storageBucket: "techweb-17c29.appspot.com",
-    messagingSenderId: "607479175084",
-    appId: "1:607479175084:web:163e9fadc2468bca908c09",
-    measurementId: "G-FYX2ZYWJL7"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyBLehoen_3ECaH49R3otGCWLpxOujgR5uc",
+  authDomain: "techweb-17c29.firebaseapp.com",
+  databaseURL: "https://techweb-17c29.firebaseio.com",
+  projectId: "techweb-17c29",
+  storageBucket: "techweb-17c29.appspot.com",
+  messagingSenderId: "607479175084",
+  appId: "1:607479175084:web:163e9fadc2468bca908c09",
+  measurementId: "G-FYX2ZYWJL7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  var userDatabase = firebase.database().ref('/users')
+var userDatabase = firebase.database().ref('/users')
 
-  const usersData = JSON.parse(localStorage.getItem('users'))
-  
-  let username = "ta"
+userDatabase.on('value', function (data) {
+  localStorage.setItem('users', JSON.stringify(Object.values(data.val())))
+})
+const usersData = JSON.parse(localStorage.getItem('users'))
 
-  // find() cua Javascript
+let user = {
+  username: "",
+  password: ""
+}
 
-  let user = usersData.find(item => item.username === username)
+function onChangeUsername(value) {
+  user.username = value
+}
 
-  console.log(user)
+function onChangePassword (value) {
+  console.log(value)
+  user.password = value
+}
+// find() cua Javascript
+
+function submit () {
+  let findUser = usersData.find(item => item.username = user.username)
+  if (findUser) {
+    if (findUser.password === user.password) {
+      alert("Login Success")
+    } else {
+      alert("Wrong Username or Password")
+    }
+  }
+}
+
+console.log(usersData)
